a8e016abd2d1e42c647bddedc8fdb48ebeb254a4
ezihe-godswill <okeybenson4@gmail.com>
2020-09-03 04:54:56 +0100

    initial commit

---
+++ tempo-weather-app/js/index.js
@@ -0,0 +1,14 @@
+// 
+window.addEventListener("load", ()=> {
+    let longitude;
+    let latitude;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            longitude =position.coords.longitude;
+            latitude = position.coords.latitude;
+
+            const api = "";
+        });
+    }
+});
